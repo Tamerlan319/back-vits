55829fd99c52811778e5764ac0be913707ed66f8
Tamerlan319 <tamerlan.soslambekov@gmail.com>
2025-06-12 16:40:45 +0300

    Инное смс3

--- server/apps/users/views.py
+++ server/apps/users/views.py
@@ -467,7 +467,7 @@ class RegisterInitView(views.APIView):
             cleaned_phone = '7' + cleaned_phone
         
         # Формируем текст сообщения
-        text = f"Привет, это сайт vits44, ваш код подтверждения: {code}"
+        text = f"Привет, это сайт vits44, ваш код подтверждения: ---{code}--- Спасибо, что вы с нами ;3"
         
         # Подготовка данных для запроса
         payload = {
