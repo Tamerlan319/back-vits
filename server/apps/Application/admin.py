684258953bc7416776c6817e686b413d57a1da65
Tamerlan319 <tamerlan.soslambekov@gmail.com>
2025-06-14 23:20:56 +0300

    добавление типов заявок7

--- server/apps/Application/admin.py
+++ server/apps/Application/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Application, ApplicationAttachment, ApplicationStatusLog
+from .models import Application, ApplicationAttachment, ApplicationStatusLog, ApplicationType
 
 admin.site.register(Application)
 admin.site.register(ApplicationAttachment)
 admin.site.register(ApplicationStatusLog)
+admin.site.register(ApplicationType)
